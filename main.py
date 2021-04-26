59b8ca7e77fee782056cf1931160811ec59cfbdc
karpi96 <arpikocsis@gmail.com>
2021-04-26 19:56:21 +0200

    organised

--- main.py
+++ main.py
@@ -14,16 +14,27 @@ FFT_data = np.fft.rfft(data)
 # Get the list of frequencies
 freq = np.fft.rfftfreq(len(data), d=1./rate)
 
-fig, axs = plt.subplots(4)
+#Add plots
+fig, axs = plt.subplots(2,2)
 fig.suptitle('FFT filter')
-axs[0].plot(time, data)
-axs[0].set_ylim([-1.5,1.5])
-
-axs[1].plot(freq, FFT_data)
+fig.tight_layout(pad=3.0)
 
 
-#generate an array, which contains the frequencies where the gaussian function will be generated
-gauss_freq = [31,62,125,250,500,1000,2000,4000,8000,16000] #10 elements
+axs[0,0].plot(time, data)
+axs[0,0].set_ylim([-1.1,1.1])
+axs[0,0].title.set_text('original signal')
+axs[0,0].set(xlabel="Time[s]", ylabel="Amplitude")
+
+axs[0,1].plot(freq, FFT_data)
+axs[0,1].title.set_text('original signal FFT')
+axs[0,1].set(xlabel="Frequency[Hz]", ylabel="Amplitude")
+
+
+
+#generate an array, 
+#which contains the frequencies where the gaussian function will be generated
+#it contains 10 elements
+gauss_freq = [31,62,125,250,500,1000,2000,4000,8000,16000] 
 
 
 #the amplitudes of the gaussian functions
@@ -41,20 +52,21 @@ amplitude_16000 = 3
 
 #an array which contains the amplitudes of the gaussian functions 
 amplitude =[amplitude_31,
-amplitude_62,
-amplitude_125,
-amplitude_250,
-amplitude_500,
-amplitude_1000,
-amplitude_2000,
-amplitude_4000,
-amplitude_8000,
-amplitude_16000]
+            amplitude_62,
+            amplitude_125,
+            amplitude_250,
+            amplitude_500,
+            amplitude_1000,
+            amplitude_2000,
+            amplitude_4000,
+            amplitude_8000,
+            amplitude_16000]
 
 
 #calculate the gaussian function
 gaussian_function = 0
 i = 0
+
 while(i<10):
     gaussian_function += amplitude[i]* np.exp(-np.square(freq-gauss_freq[i])/(2*(gauss_freq[i]/3)**2))
     i = i+1
@@ -75,8 +87,14 @@ newdata = np.fft.irfft(FFT_data)
 sf.write(file="test1.wav", data=newdata, samplerate=rate)
 
 #plot the results
-axs[2].plot(time, newdata)
-axs[2].set_ylim([-1.5,1.5])
-axs[3].plot(freq,FFT_data)
+axs[1,0].plot(time, newdata)
+axs[1,0].set_ylim([-1.1,1.1])
+axs[1,0].title.set_text('filtered signal')
+axs[1,0].set(xlabel="Time[s]", ylabel="Amplitude")
+
+
+axs[1,1].plot(freq,FFT_data)
+axs[1,1].title.set_text('filtered signal FFT')
+axs[1,1].set(xlabel="Frequency[Hz]", ylabel="Amplitude")
 
 plt.show()
